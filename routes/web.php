cb92498631c0e5e85980971df1dd5ff3656a4d59
Dinar Abdul Hollik Firdaus <dinartrevor@gmail.com>
2020-10-28 15:07:33 +0700

    Procfile

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => ['auth', 'checkRole:admin']], function () {
   // siswa
 
